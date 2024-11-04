37d77fc76677392d07334d6598390110285d7c80
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-11-04 16:52:46 +0100

    resource in ProjectController

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1,13 +1,12 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-     /**
+    /**
      * Display a listing of the resource.
      */
     public function index()
