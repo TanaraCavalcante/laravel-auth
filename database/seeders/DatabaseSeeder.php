a79e93e3019bb64e103e76406f780c47638585b5
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-11-04 17:02:48 +0100

    db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //chamo meu ProjectSeeder diretamente do db com: php artisan db:seed
+        $this -> call(ProjectSeeder::class);
     }
 }
