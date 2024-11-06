e5642e295f37b687fcd3bc3f2b9cb7c9be271029
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-11-06 01:06:22 +0100

    Show details

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -5,7 +5,11 @@
     <div class="card container p-5 my-5 col-6" >
         <div class="card-body">
         <h2 class="card-title">{{$project->title}}</h2>
-        <p class="card-text">{{$project->description}}</p>
+        <h5 class="card-text mb-4">{{$project->description}}</h5>
+        <p class="card-text"><strong>Category: </strong>{{$project->category}}</p>
+        <p class="card-text"><strong>Teck Stack: </strong>{{$project->tech_stack}}</p>
+        <p class="card-text"><strong>GitHub Link: </strong>{{$project->github_link}}</p>
+        <p class="card-text"><strong>Creation Date: </strong>{{$project->creation_date}}</p>
         </div>
     </div>
 </section>
